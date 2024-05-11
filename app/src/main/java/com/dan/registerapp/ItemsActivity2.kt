1dd60507385f013b25b9c0a3a21ae82036e1beff
Teslanot <daniilaleks8@gmail.com>
2024-05-11 20:00:46 +0300

    1.01

--- app/src/main/java/com/dan/registerapp/ItemsActivity2.kt
+++ app/src/main/java/com/dan/registerapp/ItemsActivity2.kt
@@ -1,8 +1,6 @@
 package com.dan.registerapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
